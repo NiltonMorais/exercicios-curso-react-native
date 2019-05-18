f2dfb4fab9be7062f970041418c8ec973e87cde5
Nilton Morais <niltonmorais.code@gmail.com>
2019-05-17 21:10:35 -0300

    criando primeiro component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Simples from './components/Simple';
 
 export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.f20}>App!</Text>
+                <Simples text='Flexivo!!' />
             </View>
         )
     }
